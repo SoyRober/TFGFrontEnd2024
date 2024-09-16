91a7beb6b8aeee93b4a13e1dea53db317da82814
Your Name <you@example.com>
2024-09-16 16:45:47 +0200

    setting style

--- src/routes/Settings.jsx
+++ src/routes/Settings.jsx
@@ -1,9 +1,7 @@
 import { useState, useEffect } from "react";
-import { useParams, useNavigate } from "react-router-dom";
 import { Modal, Button, Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/main.css';
-import { fetchData } from '../utils/fetch.js';
 import { jwtDecode } from 'jwt-decode'
 import EditAttributeModal from '../components/EditAttributeModal';
 import EditDateModal from '../components/EditDateModal';
@@ -27,23 +25,16 @@ export default function Settings() {
 
   useEffect(() => {
     const token = localStorage.getItem('token');
-    console.log(token);
     if (token) {
       const decodedToken = jwtDecode(token);
-      const userRole = decodedToken.role;
       setUsername(decodedToken.username)
       setEmail(decodedToken.email)
       setBirthDate(decodedToken.birthDate)
-      
-      if (userRole === "ADMIN") {
-        setRole("ADMIN");
-        setHasPermissions(true);
-      } else if (userRole === "LIBRARIAN") {
-        setRole("LIBRARIAN");
-      } else setRole("USER");
+      setRole(decodedToken.role);
+      setHasPermissions(decodedToken.role === "ADMIN");
 
     } else {
-      //TODO manage error
+      //Redirect to home or login
       console.log("Not user found")
     }
 
@@ -204,31 +195,42 @@ export default function Settings() {
     setErrorMessage('');
   };
 
-return (
-  <div>
-    <h1>Settings</h1>
-    <p>User: {username}</p>
-    <p>Email: {email}</p>
-    <p>BirthDate: {birthDate ? new Date(birthDate).toLocaleDateString() : 'N/A'}</p>
-    <p>Role: {role}</p>
-    <button className="btn btn-primary mt-3" onClick={handleEditUsername}>Edit Username</button>
-    <button className="btn btn-primary mt-3" onClick={handleEditEmail}>Edit Email</button>
-    <button className="btn btn-primary mt-3" onClick={handleEditBirthDate}>Edit BirthDate</button>
-    <button className="btn btn-secondary mt-3 ml-2" onClick={handleChangePassword}>Change Password</button>
+  return (
+    <div className="container mt-5">
+      <h1 className="text-center mb-4">Settings</h1>
+      
+      {/* User info */}
+      <div className="card shadow-sm mb-4">
+        <div className="card-body text-center">
+          <h5 className="card-title">User Information</h5>
+          <p className="card-text">Username: {username}</p>
+          <p className="card-text">Email: {email}</p>
+          <p className="card-text">BirthDate: {birthDate ? new Date(birthDate).toLocaleDateString() : 'N/A'}</p>
+          <p className="card-text">Role: {role}</p>
+        </div>
+      </div>
 
-    {/* Change username */}
-    <EditAttributeModal
-      show={showUsernameModal}
-      onClose={handleCancel}
-      attribute="Username"
-      value={newUsername}
-      onChange={(e) => setNewUsername(e.target.value)}
-      placeholder="Enter new username"
-      onSave={handleSaveUsername}
-      errorMessage={errorMessage}
-    />
+      {/* Edit buttons */}
+      <div className="d-flex flex-column align-items-center">
+        <button className="btn btn-primary mb-3 w-50" onClick={handleEditUsername}>Edit Username</button>
+        <button className="btn btn-primary mb-3 w-50" onClick={handleEditEmail}>Edit Email</button>
+        <button className="btn btn-primary mb-3 w-50" onClick={handleEditBirthDate}>Edit BirthDate</button>
+        <button className="btn btn-secondary mb-3 w-50" onClick={handleChangePassword}>Change Password</button>
+      </div>
 
-    {/* Change birthDate */}
+      {/* Change username */}
+      <EditAttributeModal
+        show={showUsernameModal}
+        onClose={handleCancel}
+        attribute="Username"
+        value={newUsername}
+        onChange={(e) => setNewUsername(e.target.value)}
+        placeholder="Enter new username"
+        onSave={handleSaveUsername}
+        errorMessage={errorMessage}
+      />
+
+      {/* Change birthDate */}
       <EditDateModal
         show={showBirthDateModal}
         onClose={handleCancel}
@@ -239,55 +241,39 @@ return (
         errorMessage={errorMessage}
       />
 
-    {/* Change Email */}
-    <EditAttributeModal
-      show={showEmailModal}
-      onClose={handleCancel}
-      attribute="Email"
-      value={newEmail}
-      onChange={(e) => setNewEmail(e.target.value)}
-      placeholder="newEmail@gmail.com"
-      onSave={handleSaveEmail}
-      errorMessage={errorMessage}
-    />
+      {/* Change Email */}
+      <EditAttributeModal
+        show={showEmailModal}
+        onClose={handleCancel}
+        attribute="Email"
+        value={newEmail}
+        onChange={(e) => setNewEmail(e.target.value)}
+        placeholder="newEmail@gmail.com"
+        onSave={handleSaveEmail}
+        errorMessage={errorMessage}
+      />
 
-    {/* Change password */}
-    <Modal show={showPasswordModal} onHide={handleCancel}>
-      <Modal.Header closeButton>
-        <Modal.Title>Change Password</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Form.Group>
-          <Form.Label>Current Password</Form.Label>
-          <Form.Control
-            type="password"
-            value={currentPassword}
-            onChange={(e) => setCurrentPassword(e.target.value)}
-            placeholder="Current Password"
-          />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>New Password</Form.Label>
-          <Form.Control
-            type="password"
-            value={newPassword}
-            onChange={(e) => setNewPassword(e.target.value)}
-            placeholder="New Password"
-          />
-        </Form.Group>
-        {errorMessage && (
-          <p style={{ color: 'red', marginTop: '10px' }}>{errorMessage}</p>
-        )}
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={handleCancel}>
-          Cancel
-        </Button>
-        <Button variant="primary" onClick={handleSavePassword}>
-          Save
-        </Button>
-      </Modal.Footer>
-    </Modal>
-  </div>
-);
+      {/* Change password */}
+      <Modal show={showPasswordModal} onHide={handleCancel}>
+        <Modal.Header closeButton>
+          <Modal.Title>Change Password</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form.Group>
+            <Form.Label>Current Password</Form.Label>
+            <Form.Control type="password" value={currentPassword} onChange={(e) => setCurrentPassword(e.target.value)} />
+          </Form.Group>
+          <Form.Group className="mt-3">
+            <Form.Label>New Password</Form.Label>
+            <Form.Control type="password" value={newPassword} onChange={(e) => setNewPassword(e.target.value)} />
+          </Form.Group>
+          {errorMessage && <p className="text-danger mt-3">{errorMessage}</p>}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCancel}>Cancel</Button>
+          <Button variant="primary" onClick={handleSavePassword}>Save</Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
 }
