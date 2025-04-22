c9dc450287767bb6da3e3a747114b68ec390fa29
SoyRober <roberlamilagrosa@gmail.com>
2025-04-22 12:14:41 +0200

    Filtro de libreria y reset de estos

--- src/components/BookCopies/ChangeAttributes.jsx
+++ src/components/BookCopies/ChangeAttributes.jsx
@@ -1,67 +1,67 @@
-import { Modal, Button, Form } from 'react-bootstrap';
+import { Modal, Button, Form } from "react-bootstrap";
 
 const ChangeLibraryModal = ({
-    showChangeLibraryModal,
-    setShowChangeLibraryModal,
-    selectedLibrary,
-    setSelectedLibrary,
-    selectedBarcode,
-    setSelectedBarcode,
-    libraries,
-    submitChangeLibrary,
+  showChangeLibraryModal,
+  setShowChangeLibraryModal,
+  selectedLibrary,
+  setSelectedLibrary,
+  selectedBarcode,
+  setSelectedBarcode,
+  libraries,
+  submitChangeLibrary,
 }) => {
-    return (
-        <Modal
-            show={showChangeLibraryModal}
-            onHide={() => setShowChangeLibraryModal(false)}
-        >
-            <Modal.Header closeButton>
-                <Modal.Title>Change Attributes</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <Form>
-                    {/* Input para cambiar el Barcode */}
-                    <Form.Group controlId="formChangeBarcode" className="mb-3">
-                        <Form.Label>Change Barcode</Form.Label>
-                        <Form.Control
-                            type="text"
-                            placeholder="Enter new barcode"
-                            value={selectedBarcode}
-                            onChange={(e) => setSelectedBarcode(e.target.value)}
-                        />
-                    </Form.Group>
+  return (
+    <Modal
+      show={showChangeLibraryModal}
+      onHide={() => setShowChangeLibraryModal(false)}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>Change Attributes</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          {/* Input para cambiar el Barcode */}
+          <Form.Group controlId="formChangeBarcode" className="mb-3">
+            <Form.Label>Change Barcode</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter new barcode"
+              value={selectedBarcode}
+              onChange={(e) => setSelectedBarcode(e.target.value)}
+            />
+          </Form.Group>
 
-                    {/* Dropdown para cambiar la Library */}
-                    <Form.Group controlId="formChangeLibrary">
-                        <Form.Label>Select New Library</Form.Label>
-                        <Form.Control
-                            as="select"
-                            value={selectedLibrary}
-                            onChange={(e) => setSelectedLibrary(e.target.value)}
-                        >
-                            <option value="">Select a library</option>
-                            {libraries.map((libraryName, index) => (
-                                <option key={index} value={libraryName}>
-                                    {libraryName}
-                                </option>
-                            ))}
-                        </Form.Control>
-                    </Form.Group>
-                </Form>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button
-                    variant="secondary"
-                    onClick={() => setShowChangeLibraryModal(false)}
-                >
-                    Close
-                </Button>
-                <Button variant="primary" onClick={submitChangeLibrary}>
-                    Save Changes
-                </Button>
-            </Modal.Footer>
-        </Modal>
-    );
+          {/* Dropdown para cambiar la Library */}
+          <Form.Group controlId="formChangeLibrary">
+            <Form.Label>Select New Library</Form.Label>
+            <Form.Control
+              as="select"
+              value={selectedLibrary}
+              onChange={(e) => setSelectedLibrary(e.target.value)}
+            >
+              <option value="">Select a library</option>
+              {libraries.map((libraryName, index) => (
+                <option key={index} value={libraryName}>
+                  {libraryName}
+                </option>
+              ))}
+            </Form.Control>
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button
+          variant="secondary"
+          onClick={() => setShowChangeLibraryModal(false)}
+        >
+          Close
+        </Button>
+        <Button variant="primary" onClick={submitChangeLibrary}>
+          Save Changes
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
 };
 
 export default ChangeLibraryModal;
--- src/routes/BookCopies.jsx
+++ src/routes/BookCopies.jsx
@@ -23,22 +23,25 @@ export default function BookCopies() {
   const [selectedBarcode, setSelectedBarcode] = useState("");
   const [showDeleteModal, setShowDeleteModal] = useState(false);
   const [barcodeFilter, setBarcodeFilter] = useState("");
+  const [libraryFilter, setLibraryFilter] = useState("");
 
   useEffect(() => {
     fetchBookCopies();
   }, [title]);
 
   useEffect(() => {
-	setPage(0);
-	setCopies([]);
+    setPage(0);
+    setCopies([]);
     fetchBookCopies();
-  }, [barcodeFilter]);
+  }, [barcodeFilter, libraryFilter]);
 
   const fetchBookCopies = async () => {
     setError(null);
     try {
       const data = await fetchData(
-        `/bookCopy?title=${encodeURIComponent(title)}&page=${page}&barcode=${barcodeFilter}`,
+        `/bookCopy?title=${encodeURIComponent(
+          title
+        )}&page=${page}&barcode=${barcodeFilter}&libraryName=${libraryFilter}`,
         "GET"
       );
 
@@ -50,7 +53,7 @@ export default function BookCopies() {
           return [...prevCopies, ...newCopies];
         });
       } else {
-        setCopies(data);
+        setCopies([]);
         toast.error(data.message);
       }
     } catch (err) {
@@ -81,7 +84,7 @@ export default function BookCopies() {
     try {
       const token = localStorage.getItem("token");
       await fetchData(
-        "/bookCopy/add",
+        "/bookCopy",
         "POST",
         {
           bookTitle: title,
@@ -181,118 +184,149 @@ export default function BookCopies() {
     return <div className="container mt-5 alert alert-danger">{error}</div>;
   }
 
-return (
-	<div className="container mt-5">
-		<div className="d-flex justify-content-between align-items-center">
-			<h1>Copies of {title}</h1>
-			<Button variant="primary" onClick={() => setShowModal(true)}>
-				Add New Copy
-			</Button>
-		</div>
-		<div className="mt-3">
-			<input
-				type="text"
-				name="barcode"
-				id="barcode"
-				placeholder="Type copy barcode"
-				className="form-control"
-				onChange={(e) => setBarcodeFilter(e.target.value)}
-			/>
-		</div>
-		<InfiniteScroll
-			dataLength={copies.length}
-			next={fetchMoreCopies}
-			hasMore={copies.length % 30 === 0}
-			loader={<Loading />}
-			endMessage={
-				<p style={{ textAlign: "center" }}>There aren't more copies</p>
-			}
-		>
-			<ul className="list-group">
-				{copies.map((copy) => (
-					<li
-						key={copy.id}
-						className="list-group-item d-flex align-items-center flex-wrap"
-						style={{ padding: "10px" }}
-					>
-						<div className="col-md-2">
-							<strong>Barcode:</strong> {copy.barcode}
-						</div>
-						<div className="col-md-2">
-							<strong>Available:</strong>{" "}
-							{copy.loanUser === "none" ? (
-								<span className="text-success">Yes</span>
-							) : (
-								<span className="text-danger">No</span>
-							)}
-						</div>
-						<div className="col-md-3">
-							<strong>Library:</strong> {copy.libraryName}
-						</div>
-						<div className="col-md-3">
-							{copy.reserveUser !== "none" ? (
-								<span className="text-warning">
-									Reserved by: {copy.reserveUser}
-								</span>
-							) : copy.loanUser !== "none" ? (
-								<span className="text-info">Loaned to: {copy.loanUser}</span>
-							) : (
-								<span>No current reservation or loan</span>
-							)}
-						</div>
-						<div className="col-md-2 text-end d-flex justify-content-between">
-							<Button
-								variant="warning"
-								size="sm"
-								className="me-1"
-								onClick={() =>
-									handleChangeLibrary(copy.id, copy.barcode, copy.libraryName)
-								}
-							>
-								Update
-							</Button>
-							<Button
-								variant="danger"
-								size="sm"
-								onClick={() => openDeleteModal(copy.id)}
-							>
-								Delete
-							</Button>
-						</div>
-					</li>
-				))}
-			</ul>
-		</InfiniteScroll>
+  return (
+    <div className="container mt-5">
+      <div className="d-flex justify-content-between align-items-center">
+        <h1>Copies of {title}</h1>
+        <Button variant="primary" onClick={() => setShowModal(true)}>
+          Add New Copy
+        </Button>
+      </div>
+      <div className="mt-3 d-flex align-items-center">
+        <input
+          type="text"
+          name="barcode"
+          id="barcode"
+          placeholder="Type copy barcode"
+          className="form-control me-2"
+          value={barcodeFilter}
+          onChange={(e) => setBarcodeFilter(e.target.value)}
+        />
+        <Button
+          variant="secondary"
+          size="sm"
+          className="me-3"
+          onClick={() => setBarcodeFilter("")}
+        >
+          Reset
+        </Button>
+        <select
+          id="librarySelect"
+          className="form-control me-2"
+          value={libraryFilter}
+          onChange={(e) => setLibraryFilter(e.target.value)}
+        >
+          <option value="">Select a library</option>
+          {libraries.map((libraryName, index) => (
+            <option key={index} value={libraryName}>
+              {libraryName}
+            </option>
+          ))}
+        </select>
+        <Button
+          variant="secondary"
+          size="sm"
+          className="me-3"
+          onClick={() => setLibraryFilter("")}
+        >
+          Reset
+        </Button>
+      </div>
 
-		{/* Delete Confirmation Modal */}
-		<DeleteConfirmationModal
-			show={showDeleteModal}
-			onClose={closeDeleteModal}
-			onDelete={handleDeleteCopy}
-			message="Are you sure you want to delete this copy?"
-		/>
+      <InfiniteScroll
+        dataLength={copies.length}
+        next={fetchMoreCopies}
+        hasMore={copies.length % 30 === 0 && copies.length > 0}
+        loader={<Loading />}
+        endMessage={
+          <p className="text-center my-2">There aren't more copies</p>
+        }
+      >
+        <ul className="list-group mt-3">
+          {copies.map((copy) => (
+            <li
+              key={copy.id}
+              className="list-group-item d-flex align-items-center flex-wrap"
+              style={{ padding: "10px" }}
+            >
+              <div className="col-md-2">
+                <strong>Barcode:</strong> {copy.barcode}
+              </div>
+              <div className="col-md-2">
+                <strong>Available:</strong>{" "}
+                {copy.loanUser === "none" ? (
+                  <span className="text-success">Yes</span>
+                ) : (
+                  <span className="text-danger">No</span>
+                )}
+              </div>
+              <div className="col-md-3">
+                <strong>Library:</strong> {copy.libraryName}
+              </div>
+              <div className="col-md-3">
+                {copy.reserveUser !== "none" ? (
+                  <span className="text-warning">
+                    Reserved by: {copy.reserveUser}
+                  </span>
+                ) : copy.loanUser !== "none" ? (
+                  <span className="text-info">Loaned to: {copy.loanUser}</span>
+                ) : (
+                  <span>No current reservation or loan</span>
+                )}
+              </div>
+              <div className="col-md-2 text-end d-flex justify-content-between">
+                <Button
+                  variant="warning"
+                  size="sm"
+                  className="me-1"
+                  onClick={() =>
+                    handleChangeLibrary(copy.id, copy.barcode, copy.libraryName)
+                  }
+                >
+                  Update
+                </Button>
+                <Button
+                  variant="danger"
+                  size="sm"
+                  onClick={() => openDeleteModal(copy.id)}
+                >
+                  Delete
+                </Button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </InfiniteScroll>
 
-		{/* Add New Copy Modal */}
-		<CreateCopyModal
-			showModal={showModal}
-			setShowModal={setShowModal}
-			newCopy={newCopy}
-			setNewCopy={setNewCopy}
-			libraries={libraries}
-			handleAddCopy={handleAddCopy}
-		/>
+      {/* Delete Confirmation Modal */}
+      <DeleteConfirmationModal
+        show={showDeleteModal}
+        onClose={closeDeleteModal}
+        onDelete={handleDeleteCopy}
+        message="Are you sure you want to delete this copy?"
+      />
 
-		{/* Change Library Modal */}
-		<ChangeLibraryModal
-			showChangeLibraryModal={showChangeLibraryModal}
-			setShowChangeLibraryModal={setShowChangeLibraryModal}
-			selectedLibrary={selectedLibrary}
-			setSelectedLibrary={setSelectedLibrary}
-			selectedBarcode={selectedBarcode}
-			setSelectedBarcode={setSelectedBarcode}
-			libraries={libraries}
-			submitChangeLibrary={handleUpdate}
-		/>
-	</div>
-);
+      {/* Add New Copy Modal */}
+      <CreateCopyModal
+        showModal={showModal}
+        setShowModal={setShowModal}
+        newCopy={newCopy}
+        setNewCopy={setNewCopy}
+        libraries={libraries}
+        handleAddCopy={handleAddCopy}
+      />
+
+      {/* Change Library Modal */}
+      <ChangeLibraryModal
+        showChangeLibraryModal={showChangeLibraryModal}
+        setShowChangeLibraryModal={setShowChangeLibraryModal}
+        selectedLibrary={selectedLibrary}
+        setSelectedLibrary={setSelectedLibrary}
+        selectedBarcode={selectedBarcode}
+        setSelectedBarcode={setSelectedBarcode}
+        libraries={libraries}
+        submitChangeLibrary={handleUpdate}
+      />
+    </div>
+  );
 }
