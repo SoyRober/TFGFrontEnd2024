d6c241b1d5f6ce491b7507875a6e3494bd2f574e
SoyRober <roberlamilagrosa@gmail.com>
2025-04-22 19:05:13 +0200

    Bilbioteca en selector constantemente

--- src/components/LibrarySelector.jsx
+++ src/components/LibrarySelector.jsx
@@ -3,44 +3,46 @@ import { fetchData } from "../utils/fetch";
 import { useNavigate } from "react-router-dom";
 
 const LibrarySelector = () => {
-	const [libraries, setLibraries] = useState([]);
-	const [selectedLibrary, setSelectedLibrary] = useState("");
+    const [libraries, setLibraries] = useState([]);
+    const [selectedLibrary, setSelectedLibrary] = useState(
+        localStorage.getItem("libraryName") || ""
+    );
 
-	const navigate = useNavigate();
+    const navigate = useNavigate();
 
-	useEffect(() => {
-		const fetchLibraries = async () => {
-			const data = await fetchData("/libraries/list", "GET");
-			setLibraries(data);
-		};
-		fetchLibraries();
-	}, []);
+    useEffect(() => {
+        const fetchLibraries = async () => {
+            const data = await fetchData("/libraries/list", "GET");
+            setLibraries(data);
+        };
+        fetchLibraries();
+    }, []);
 
-	const handleLibraryChange = async (e) => {
-		const selectedLibraryName = e.target.value;
-		localStorage.setItem("libraryName", selectedLibraryName);
-		setSelectedLibrary(selectedLibraryName);
-		navigate("/");
-	};
+    const handleLibraryChange = async (e) => {
+        const selectedLibraryName = e.target.value;
+        localStorage.setItem("libraryName", selectedLibraryName);
+        setSelectedLibrary(selectedLibraryName);
+        navigate("/");
+    };
 
-	return (
-		<div>
-			<select
-				className="form-select bg-dark text-light"
-				value={selectedLibrary}
-				onChange={handleLibraryChange}
-			>
-				<option key="default" value="" disabled>
-					Select a library
-				</option>
-				{libraries.map((library) => (
-					<option key={library} value={library}>
-						{library}
-					</option>
-				))}
-			</select>
-		</div>
-	);
+    return (
+        <div>
+            <select
+                className="form-select bg-dark text-light"
+                value={selectedLibrary}
+                onChange={handleLibraryChange}
+            >
+                <option key="default" value="" disabled>
+                    Select a library
+                </option>
+                {libraries.map((library) => (
+                    <option key={library} value={library}>
+                        {library}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
 };
 
 export default LibrarySelector;
