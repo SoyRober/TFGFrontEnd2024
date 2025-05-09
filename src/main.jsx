d4da512fd25414e634cdfec5d8bf4f571734902b
SoyRober <roberlamilagrosa@gmail.com>
2025-05-09 18:06:42 +0200

    Ruta para busqueda de libros

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { createRoot } from "react-dom/client";
-import LibraryHomepage from "./routes/LibraryHomepage.jsx";
+import Search from "./routes/Search.jsx";
 import Presentation from "./routes/Presentation.jsx";
 import Navbar from "./components/navbar/Navbar.jsx";
 import Login from "./routes/Login.jsx";
@@ -19,78 +19,78 @@ import BookCopies from "./routes/BookCopies.jsx";
 import Libraries from "./routes/Libraries.jsx";
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <Navbar />,
-		children: [
-			{
-				path: "/",
-				element: <Presentation />,
-			},
-			{
-				path: "/libraryHomepage",
-				element: <LibraryHomepage />,
-			},
-			{
-				path: "/login",
-				element: <Login />,
-			},
-			{
-				path: "/register",
-				element: <Register />,
-			},
-			{
-				path: "/user/userSettings",
-				element: <UserSettings />,
-			},
-			{
-				path: "/user/loans",
-				element: <Loans />,
-			},
-			{
-				path: "/viewBook/:title",
-				element: <ViewBook />,
-			},
-			{
-				path: "/usersList",
-				element: <UsersList />,
-			},
-			{
-				path: "/profile/:email",
-				element: <ViewProfile />,
-			},
-			{
-				path: "/userBookDetails",
-				element: <UserBooksDetails />,
-			},
-			{
-				path: "/attributes",
-				element: <Attributes />,
-			},
-			{
-				path: "/genres",
-				element: <Genres />,
-			},
-			{
-				path: "/authors",
-				element: <Authors />,
-			},
-			{
-				path: "/pendingBooks",
-				element: <PendingBooks />,
-			},
-			{
-				path: "/bookCopies/:title",
-				element: <BookCopies />,
-			},
-			{
-				path: "/libraries",
-				element: <Libraries />,
-			},
-		],
-	},
+  {
+    path: "/",
+    element: <Navbar />,
+    children: [
+      {
+        path: "/",
+        element: <Presentation />,
+      },
+      {
+        path: "/search",
+        element: <Search />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/user/userSettings",
+        element: <UserSettings />,
+      },
+      {
+        path: "/user/loans",
+        element: <Loans />,
+      },
+      {
+        path: "/viewBook/:title",
+        element: <ViewBook />,
+      },
+      {
+        path: "/usersList",
+        element: <UsersList />,
+      },
+      {
+        path: "/profile/:email",
+        element: <ViewProfile />,
+      },
+      {
+        path: "/userBookDetails",
+        element: <UserBooksDetails />,
+      },
+      {
+        path: "/attributes",
+        element: <Attributes />,
+      },
+      {
+        path: "/genres",
+        element: <Genres />,
+      },
+      {
+        path: "/authors",
+        element: <Authors />,
+      },
+      {
+        path: "/pendingBooks",
+        element: <PendingBooks />,
+      },
+      {
+        path: "/bookCopies/:title",
+        element: <BookCopies />,
+      },
+      {
+        path: "/libraries",
+        element: <Libraries />,
+      },
+    ],
+  },
 ]);
 
 createRoot(document.getElementById("root")).render(
-	<RouterProvider router={router}></RouterProvider>
+  <RouterProvider router={router}></RouterProvider>
 );
