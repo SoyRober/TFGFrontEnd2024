98e1557cc6114fa937f2003b99bf1b2bb40ad09d
SoyRober <roberlamilagrosa@gmail.com>
2025-05-12 16:50:21 +0200

    Navbar con estructura de listado correcta

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -66,42 +66,29 @@ export default function Root() {
             Home
           </Link>
 
-          <li>
-            <Link
-              className="nav-link text-light ms-3"
-              to="/search"
-              aria-label="Search Link"
-            >
-              Search
-            </Link>
-          </li>
-
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav me-auto">
-              {isLoggedIn && (
-                <>
-                  <li className="nav-item">
-                    <Link
-                      className="nav-link text-light ms-3"
-                      to="/userBookDetails"
-                      aria-label="My Books Link"
-                    >
-                      My books
-                    </Link>
-                  </li>
-                  {hasPermissions && (
+          <ul className="navbar-nav me-auto">
+            <li className="nav-item">
+              <Link
+                className="nav-link text-light ms-3"
+                to="/search"
+                aria-label="Search Link"
+              >
+                Search
+              </Link>
+            </li>
+            {isLoggedIn && (
+              <>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link text-light ms-3"
+                    to="/userBookDetails"
+                    aria-label="My Books Link"
+                  >
+                    My books
+                  </Link>
+                </li>
+                {hasPermissions && (
+                  <>
                     <li className="nav-item">
                       <Link
                         className="nav-link text-light ms-3"
@@ -111,8 +98,6 @@ export default function Root() {
                         Users List
                       </Link>
                     </li>
-                  )}
-                  {hasPermissions && (
                     <li className="nav-item">
                       <Link
                         className="nav-link text-light ms-3"
@@ -122,19 +107,15 @@ export default function Root() {
                         Attributes
                       </Link>
                     </li>
-                  )}
-                  {hasPermissions && (
                     <li className="nav-item">
                       <Link
                         className="nav-link text-light ms-3"
                         to="/pendingBooks"
                         aria-label="Pending Books Link"
                       >
-                        PendingBooks
+                        Pending Books
                       </Link>
                     </li>
-                  )}
-                  {hasPermissions && (
                     <li className="nav-item">
                       <Link
                         className="nav-link text-light ms-3"
@@ -144,11 +125,14 @@ export default function Root() {
                         Libraries
                       </Link>
                     </li>
-                  )}
-                </>
-              )}
-            </ul>
-            {!isLoggedIn && (
+                  </>
+                )}
+              </>
+            )}
+          </ul>
+
+          <div className="collapse navbar-collapse" id="navbarNav">
+            {!isLoggedIn ? (
               <ul className="navbar-nav ms-auto">
                 <li className="nav-item me-3">
                   <LibrarySelector />
@@ -172,60 +156,58 @@ export default function Root() {
                   </Link>
                 </li>
               </ul>
+            ) : (
+              <div className="d-flex align-items-center ms-auto">
+                <ul className="navbar-nav d-flex flex-row align-items-center">
+                  <li className="nav-item me-3">
+                    <LibrarySelector />
+                  </li>
+                  <li className="nav-item me-3">
+                    <Notifications />
+                  </li>
+                  <li className="nav-item dropdown">
+                    <button
+                      className="nav-link dropdown-toggle text-light bg-dark"
+                      id="navbarDropdown"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                      aria-label="Profile Dropdown Button"
+                    >
+                      <span className="d-none d-lg-inline">Profile</span>
+                      <i className="fas fa-user d-lg-none"></i>
+                    </button>
+                    <ul
+                      className="dropdown-menu dropdown-menu-end bg-dark"
+                      aria-labelledby="navbarDropdown"
+                      style={{
+                        position: "absolute",
+                        zIndex: 1050,
+                      }}
+                    >
+                      <li>
+                        <Link
+                          className="dropdown-item text-light hover-navbar"
+                          to="/user/userSettings"
+                          aria-label="Settings Link"
+                        >
+                          Settings
+                        </Link>
+                      </li>
+                      <li>
+                        <button
+                          className="dropdown-item text-light hover-navbar"
+                          onClick={handleLogout}
+                          aria-label="Logout Button"
+                        >
+                          Log out
+                        </button>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
             )}
           </div>
-
-          {isLoggedIn && (
-            <div className="d-flex align-items-center ms-auto">
-              <ul className="navbar-nav d-flex flex-row align-items-center">
-                <li className="nav-item me-3">
-                  <LibrarySelector />
-                </li>
-                <li className="nav-item me-3">
-                  <Notifications />
-                </li>
-                <li className="nav-item dropdown">
-                  <button
-                    className="nav-link dropdown-toggle text-light bg-dark"
-                    id="navbarDropdown"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                    aria-label="Profile Dropdown Button"
-                  >
-                    <span className="d-none d-lg-inline">Profile</span>
-                    <i className="fas fa-user d-lg-none"></i>
-                  </button>
-                  <ul
-                    className="dropdown-menu dropdown-menu-end bg-dark"
-                    aria-labelledby="navbarDropdown"
-                    style={{
-                      position: "absolute",
-                      zIndex: 1050,
-                    }}
-                  >
-                    <li>
-                      <Link
-                        className="dropdown-item text-light hover-navbar"
-                        to="/user/userSettings"
-                        aria-label="Settings Link"
-                      >
-                        Settings
-                      </Link>
-                    </li>
-                    <li>
-                      <button
-                        className="dropdown-item text-light hover-navbar"
-                        onClick={handleLogout}
-                        aria-label="Logout Button"
-                      >
-                        Log out
-                      </button>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </div>
-          )}
         </div>
       </nav>
       <Outlet />
