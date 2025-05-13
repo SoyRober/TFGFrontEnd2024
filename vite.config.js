dc3f77f3eaaea31ca83652e1be6aa3e30201f676
SoyRober <roberlamilagrosa@gmail.com>
2025-05-13 10:44:02 +0200

    Quitar configuracion y prioridad en carga

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import compress from 'vite-plugin-compress'; 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    compress({
-      brotli: true,
-      gzip: true,
-    }),
-  ],
-  build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true, 
-        drop_debugger: true, 
-      },
-      mangle: true, 
-    },
-    rollupOptions: {
-      output: {
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]',
-      },
-    },
-  },
+  plugins: [react()],
 });
