76211e8d92ebf692314ff5986a6c56309371fe84
SoyRober <roberlamilagrosa@gmail.com>
2025-05-14 18:42:02 +0200

    Rutas con code splitting

--- src/main.jsx
+++ src/main.jsx
@@ -1,45 +1,52 @@
 import { createHashRouter, RouterProvider } from "react-router-dom";
 import { createRoot } from "react-dom/client";
+import { Suspense, lazy } from "react";
 
-import Search from "./routes/Search.jsx";
-import Presentation from "./routes/Presentation.jsx";
-import Navbar from "./components/navbar/Navbar.jsx";
-import Login from "./routes/Login.jsx";
-import Register from "./routes/Register.jsx";
-import ViewBook from "./routes/ViewBook.jsx";
-import Loans from "./routes/Loans.jsx";
-import UserSettings from "./routes/UserSettings.jsx";
-import UsersList from "./routes/UsersList.jsx";
-import ViewProfile from "./routes/ViewProfile.jsx";
-import UserBooksDetails from "./routes/UserBooksDetails.jsx";
-import Attributes from "./routes/Attributes.jsx";
-import Genres from "./routes/Genres.jsx";
-import Authors from "./routes/Authors.jsx";
-import PendingBooks from "./routes/PendingBooks.jsx";
-import BookCopies from "./routes/BookCopies.jsx";
-import Libraries from "./routes/Libraries.jsx";
+const Search = lazy(() => import("./routes/Search.jsx"));
+const Presentation = lazy(() => import("./routes/Presentation.jsx"));
+const Navbar = lazy(() => import("./components/navbar/Navbar.jsx"));
+const Login = lazy(() => import("./routes/Login.jsx"));
+const Register = lazy(() => import("./routes/Register.jsx"));
+const ViewBook = lazy(() => import("./routes/ViewBook.jsx"));
+const Loans = lazy(() => import("./routes/Loans.jsx"));
+const UserSettings = lazy(() => import("./routes/UserSettings.jsx"));
+const UsersList = lazy(() => import("./routes/UsersList.jsx"));
+const ViewProfile = lazy(() => import("./routes/ViewProfile.jsx"));
+const UserBooksDetails = lazy(() => import("./routes/UserBooksDetails.jsx"));
+const Attributes = lazy(() => import("./routes/Attributes.jsx"));
+const Genres = lazy(() => import("./routes/Genres.jsx"));
+const Authors = lazy(() => import("./routes/Authors.jsx"));
+const PendingBooks = lazy(() => import("./routes/PendingBooks.jsx"));
+const BookCopies = lazy(() => import("./routes/BookCopies.jsx"));
+const Libraries = lazy(() => import("./routes/Libraries.jsx"));
+
+const withSuspense = (Component) => (
+  <Suspense fallback={<div>Loading...</div>}>
+    <Component />
+  </Suspense>
+);
 
 const router = createHashRouter([
   {
-    path: "/",             // raíz: “/#/…”
-    element: <Navbar />,
+    path: "/",
+    element: withSuspense(Navbar),
     children: [
-      { index: true, element: <Presentation /> },   // ruta “#/”
-      { path: "search",         element: <Search /> },           // “#/search”
-      { path: "login",          element: <Login /> },            // “#/login”
-      { path: "register",       element: <Register /> },
-      { path: "user/userSettings", element: <UserSettings /> },
-      { path: "user/loans",     element: <Loans /> },
-      { path: "viewBook/:title", element: <ViewBook /> },
-      { path: "usersList",      element: <UsersList /> },
-      { path: "profile/:email", element: <ViewProfile /> },
-      { path: "userBookDetails",element: <UserBooksDetails /> },
-      { path: "attributes",     element: <Attributes /> },
-      { path: "genres",         element: <Genres /> },
-      { path: "authors",        element: <Authors /> },
-      { path: "pendingBooks",   element: <PendingBooks /> },
-      { path: "bookCopies/:title", element: <BookCopies /> },
-      { path: "libraries",      element: <Libraries /> },
+      { index: true, element: withSuspense(Presentation) },
+      { path: "search", element: withSuspense(Search) },
+      { path: "login", element: withSuspense(Login) },
+      { path: "register", element: withSuspense(Register) },
+      { path: "user/userSettings", element: withSuspense(UserSettings) },
+      { path: "user/loans", element: withSuspense(Loans) },
+      { path: "viewBook/:title", element: withSuspense(ViewBook) },
+      { path: "usersList", element: withSuspense(UsersList) },
+      { path: "profile/:email", element: withSuspense(ViewProfile) },
+      { path: "userBookDetails", element: withSuspense(UserBooksDetails) },
+      { path: "attributes", element: withSuspense(Attributes) },
+      { path: "genres", element: withSuspense(Genres) },
+      { path: "authors", element: withSuspense(Authors) },
+      { path: "pendingBooks", element: withSuspense(PendingBooks) },
+      { path: "bookCopies/:title", element: withSuspense(BookCopies) },
+      { path: "libraries", element: withSuspense(Libraries) },
     ],
   },
 ]);
