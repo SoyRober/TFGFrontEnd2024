554be52c898c910c11647639cba7cb8ce5623104
SoyRober <roberlamilagrosa@gmail.com>
2025-05-15 18:19:42 +0200

    Recolocar imports

--- src/components/BookCardsLoan.jsx
+++ src/components/BookCardsLoan.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import defaultBook from "/img/defaultBook.svg";
 
 const BookCardLoans = React.memo(({ loan, cardSize, isFirst }) => {
   const getColumnClass = (size) => {
