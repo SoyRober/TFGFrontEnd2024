8937e739b713917294f7443ba37b9109d6bc51d7
SoyRober <roberlamilagrosa@gmail.com>
2025-05-16 00:18:25 +0200

    Minify mas sencillo

--- vite.config.js
+++ vite.config.js
@@ -1,61 +1,39 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import { visualizer } from "rollup-plugin-visualizer";
-import lightningcss from "vite-plugin-lightningcss";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
   plugins: [
     react(),
-    lightningcss({
-      browserslist: ['defaults'],
-      lightningcssOptions: {
-        minify: true,
-        targets: {
-          chrome: 100,
-          firefox: 100,
-        },
-        drafts: {
-          nesting: true,
-        },
-      },
+    visualizer({
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
     }),
-    // visualizer({
-    //   open: true,
-    //   gzipSize: true,
-    //   brotliSize: true,
-    // }),
   ],
   build: {
     minify: 'terser',
     terserOptions: {
       compress: {
-        passes: 3,
-        pure_funcs: ['console.log', 'console.info', 'console.debug'],
         drop_console: true,
         drop_debugger: true,
-        booleans_as_integers: true,
-        ecma: 2020,
-        module: true,
-        toplevel: true,
-        hoist_funs: true,
-        hoist_props: true,
-        hoist_vars: true,
-        inline: true,
-        reduce_funcs: true,
+        pure_funcs: ['console.info', 'console.debug', 'console.warn'], 
+        passes: 3,
+        unsafe: true, 
+        unsafe_arrows: true,
+        unsafe_methods: true,
+        unsafe_proto: true,
+        unsafe_comps: true,
+        toplevel: true, 
+        collapse_vars: true,
         reduce_vars: true,
-        switches: true,
       },
       mangle: {
-        toplevel: true,
-        properties: {
-          regex: /^_/,
-        },
+        toplevel: true, 
       },
       format: {
         comments: false,
-        ascii_only: true,
       },
     },
-    cssMinify: false, 
   },
 });
