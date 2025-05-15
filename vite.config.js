d3f3f3acb12f062d6ef35da671c55e6329905fad
SoyRober <roberlamilagrosa@gmail.com>
2025-05-15 23:47:35 +0200

    MInify con terser

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+// import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    // visualizer({
+    //   open: true,
+    //   gzipSize: true,
+    //   brotliSize: true,
+    // }),
+  ],
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+      format: {
+        comments: false,
+      },
+    },
+  },
 });
