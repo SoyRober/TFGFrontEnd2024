31714a761d9949f61a6704c6b5b88fe6ae325ab6
SoyRober <roberlamilagrosa@gmail.com>
2025-05-27 16:38:16 +0200

    Quitar mailgun para contraseña y mejor css

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -3,169 +3,181 @@ import { fetchData } from "../utils/fetch.js";
 import { useNavigate, useLocation } from "react-router-dom";
 import { toast } from "react-toastify";
 import InfoModal from "../components/modals/InfoModal.jsx";
-import PasswordRecoveryForm from "../components/PasswordRecoveryForm.jsx";
 
 const Login = () => {
-	const [password, setPassword] = useState("");
-	const [username, setUsername] = useState("");
-	const [showModal, setShowModal] = useState(false);
-	const [modalTitle, setModalTitle] = useState("");
-	const [modalContent, setModalContent] = useState(null);
-	const navigate = useNavigate();
-	const location = useLocation();
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [showModal, setShowModal] = useState(false);
+  const [modalTitle, setModalTitle] = useState("");
+  const [modalContent, setModalContent] = useState(null);
+  const navigate = useNavigate();
+  const location = useLocation();
 
-	const openReactivationModal = () => {
-		setModalTitle("Account Reactivation");
-		setModalContent(
-			<>
-				If you deactivated your account and want it back, contact our support
-				and send your username and email.
-				<br />
-				<a
-					href="mailto:bibliosupport@gmail.com?subject=Reactivation Request"
-					className="text-info"
-					target="_blank"
-				>
-					bibliosupport@gmail.com
-				</a>
-			</>
-		);
-		setShowModal(true);
-	};
+  const openReactivationModal = () => {
+    setModalTitle("Account Reactivation");
+    setModalContent(
+      <>
+        If you deactivated your account and want it back, contact our support
+        and send your username and email.
+        <br />
+        <a
+          href="mailto:bibliosupport@gmail.com?subject=Reactivation Request"
+          aria-label="Send an email to Bibliosupport for reactivation"
+          className="text-info"
+          target="_blank"
+        >
+          biliceu.soporte@gmail.com
+        </a>
+      </>
+    );
+    setShowModal(true);
+  };
 
-	const openPasswordRecoveryModal = () => {
-		setModalTitle("Password Recovery");
-		setModalContent(
-			<PasswordRecoveryForm onClose={() => setShowModal(false)} />
-		);
-		setShowModal(true);
-	};
+  const openPasswordRecoveryModal = () => {
+    setModalTitle("Account Recovery");
+    setModalContent(
+      <>
+        If you want to recover your account, send your username and email to our
+        support.
+        <br />
+        <a
+          href="mailto:bibliosupport@gmail.com?subject=Password Recovery Request"
+          aria-label="Send an email to Bibliosupport for password recovery"
+          className="text-info"
+          target="_blank"
+        >
+          biliceu.soporte@gmail.com
+        </a>
+      </>
+    );
+    setShowModal(true);
+  };
 
-	const handleHideModal = () => setShowModal(false);
+  const handleHideModal = () => setShowModal(false);
 
-	useEffect(() => {
-		const params = new URLSearchParams(location.search);
-		const error = params.get("error");
-		if (error === "google_auth") {
-			toast.error(
-				"Google authentication failed. Check if you have an active account or contact support"
-			);
-		}
-	}, [location]);
+  useEffect(() => {
+    const params = new URLSearchParams(location.search);
+    const error = params.get("error");
+    if (error === "google_auth") {
+      toast.error(
+        "Google authentication failed. Check if you have an active account or contact support"
+      );
+    }
+  }, [location]);
 
-	const handleLogin = async (e) => {
-		e.preventDefault();
-		const userData = { username, password };
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const userData = { username, password };
 
-		try {
-			const response = await fetchData("/public/users/login", "POST", userData);
-			if (response.success) {
-				localStorage.setItem("token", response.message);
-				toast.success("Logged in successfully");
-				navigate("/");
-			} else {
-				toast.error(response.message || "Login error. Please try again.");
-			}
-		} catch (error) {
-			toast.error(error.message || "Login error. Please try again.");
-		}
-	};
+    try {
+      const response = await fetchData("/public/users/login", "POST", userData);
+      if (response.success) {
+        localStorage.setItem("token", response.message);
+        toast.success("Logged in successfully");
+        navigate("/");
+      } else {
+        toast.error(response.message || "Login error. Please try again.");
+      }
+    } catch (error) {
+      toast.error(error.message || "Login error. Please try again.");
+    }
+  };
 
-	const handleGoogleLogin = () => {
-		const googleClientId =
-			"115537997590-sefunkh5od17kagalf4747ov5trjt387.apps.googleusercontent.com";
-		const redirectUri = "https://biliceu.store/oauth/callback";
-		const authUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${googleClientId}&redirect_uri=${redirectUri}&response_type=code&scope=openid%20email%20profile`;
+  const handleGoogleLogin = () => {
+    const googleClientId =
+      "115537997590-sefunkh5od17kagalf4747ov5trjt387.apps.googleusercontent.com";
+    const redirectUri = "https://biliceu.store/oauth/callback";
+    const authUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${googleClientId}&redirect_uri=${redirectUri}&response_type=code&scope=openid%20email%20profile`;
 
-		window.location.href = authUrl;
-	};
+    window.location.href = authUrl;
+  };
 
-	return (
-		<main className="container mt-5" role="main">
-			<div className="row justify-content-center">
-				<div className="col-md-6 col-lg-4">
-					<div className="card shadow-lg" aria-labelledby="loginTitle">
-						<div className="card-body">
-							<h2 id="loginTitle" className="card-title text-center mb-4">
-								Log In
-							</h2>
-							<form onSubmit={handleLogin}>
-								<div className="mb-3">
-									<label htmlFor="username" className="form-label">
-										Username:
-									</label>
-									<input
-										type="text"
-										className="form-control shadow-sm"
-										id="username"
-										value={username}
-										onChange={(e) => setUsername(e.target.value)}
-										aria-describedby="usernameHelp"
-										required
-									/>
-								</div>
-								<div className="mb-3">
-									<label htmlFor="password" className="form-label">
-										Password:
-									</label>
-									<input
-										type="password"
-										className="form-control shadow-sm"
-										id="password"
-										value={password}
-										onChange={(e) => setPassword(e.target.value)}
-										aria-describedby="passwordHelp"
-										required
-									/>
-								</div>
-								<button
-									type="submit"
-									className="btn btn-primary w-100 shadow-sm"
-									aria-label="Log in to your account"
-								>
-									Log In
-								</button>
-								<button
-									type="button"
-									className="btn btn-danger w-100 shadow-sm mt-2"
-									onClick={handleGoogleLogin}
-									aria-label="Log in using your Google account"
-								>
-									Log in with Google
-								</button>
-								<div className="mt-3 text-center">
-									<button
-										type="button"
-										onClick={openReactivationModal}
-										className="btn btn-link p-0 me-3"
-										aria-label="View information about account reactivation"
-									>
-										Reactivation Info
-									</button>
+  return (
+    <main className="container mt-5" role="main">
+      <div className="row justify-content-center">
+        <div className="col-md-6 col-lg-4">
+          <div className="card shadow-lg" aria-labelledby="loginTitle">
+            <div className="card-body">
+              <h2 id="loginTitle" className="card-title text-center mb-4">
+                Log In
+              </h2>
+              <form onSubmit={handleLogin}>
+                <div className="mb-3">
+                  <label htmlFor="username" className="form-label">
+                    Username:
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control shadow-sm"
+                    id="username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    aria-describedby="usernameHelp"
+                    required
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="password" className="form-label">
+                    Password:
+                  </label>
+                  <input
+                    type="password"
+                    className="form-control shadow-sm"
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    aria-describedby="passwordHelp"
+                    required
+                  />
+                </div>
+                <button
+                  type="submit"
+                  className="btn btn-primary w-100 shadow-sm"
+                  aria-label="Log in to your account"
+                >
+                  Log In
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-danger w-100 shadow-sm mt-2"
+                  onClick={handleGoogleLogin}
+                  aria-label="Log in using your Google account"
+                >
+                  Log in with Google
+                </button>
+                <div className="mt-3 d-flex justify-content-center gap-3 flex-wrap">
+                  <button
+                    type="button"
+                    onClick={openReactivationModal}
+                    className="btn btn-link p-0"
+                    aria-label="View information about account reactivation"
+                  >
+                    Reactivation Info
+                  </button>
 
-									<button
-										type="button"
-										onClick={openPasswordRecoveryModal}
-										className="btn btn-link p-0"
-										aria-label="View information about password recovery"
-									>
-										Forgot password?
-									</button>
-								</div>
-							</form>
-						</div>
-					</div>
-				</div>
-			</div>
+                  <button
+                    type="button"
+                    onClick={openPasswordRecoveryModal}
+                    className="btn btn-link p-0"
+                    aria-label="View information about password recovery"
+                  >
+                    Forgot password?
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
 
-			<InfoModal
-				show={showModal}
-				handleClose={handleHideModal}
-				title={modalTitle}
-				content={modalContent}
-			/>
-		</main>
-	);
+      <InfoModal
+        show={showModal}
+        handleClose={handleHideModal}
+        title={modalTitle}
+        content={modalContent}
+      />
+    </main>
+  );
 };
 
 export default Login;
