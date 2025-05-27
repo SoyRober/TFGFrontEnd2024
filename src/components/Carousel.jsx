3ce0de63e72eca655a20bc785639364b87598955
SoyRober <roberlamilagrosa@gmail.com>
2025-05-27 22:56:10 +0200

    Car diferente de imagen

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -32,6 +32,7 @@ const fetchBooks = async (setBooks, genre = "", library) => {
 
 const Card = React.memo(function Card({ title, image, preload }) {
   const [isLoading, setIsLoading] = useState(true);
+  const [imageSrc, setImageSrc] = useState(defaultBook);
   const navigate = useNavigate();
 
   const navigateToBookDetails = () => {
@@ -41,7 +42,21 @@ const Card = React.memo(function Card({ title, image, preload }) {
 
   const handleImageLoad = () => {
     setIsLoading(false);
+    setImageSrc(image);
   };
+
+  useEffect(() => {
+    if (preload) {
+      requestAnimationFrame(() => {
+        setIsLoading(false);
+      });
+    } else {
+      const timeout = setTimeout(() => {
+        setIsLoading(false);
+      }, Math.random() * (700 - 300) + 300);
+      return () => clearTimeout(timeout);
+    }
+  }, [preload]);
 
   return (
     <div
@@ -55,35 +70,37 @@ const Card = React.memo(function Card({ title, image, preload }) {
       }}
       aria-label={`See book details of ${title}`}
     >
-      {isLoading && (
+      {isLoading ? (
         <div>
           <Skeleton width={200} height={20} className="my-3" />
           <Skeleton width={300} height={375} />
         </div>
+      ) : (
+        <>
+          <h2 title={title}>
+            {title.length > 30 ? title.slice(0, 30) + "..." : title}
+          </h2>
+          <img
+            src={imageSrc}
+            alt={`Book front page of ${title}`}
+            loading={preload ? "eager" : "lazy"}
+            onLoad={handleImageLoad}
+            style={{
+              width: "100%",
+              height: "auto",
+              aspectRatio: "2 / 3",
+              objectFit: "contain",
+              maxWidth: "220px",
+              display: "block",
+              margin: "0 auto",
+            }}
+            className="shadow"
+          />
+        </>
       )}
-      <h2 title={title} style={{ display: isLoading ? "none" : "block" }}>
-        {title.length > 30 ? title.slice(0, 30) + "..." : title}
-      </h2>
-      <img
-        src={image || defaultBook}
-        alt={`Book front page of ${title}`}
-        loading={preload ? "eager" : "lazy"}
-        onLoad={handleImageLoad}
-        style={{
-          width: "100%",
-          height: "auto",
-          aspectRatio: "2 / 3",
-          objectFit: "contain",
-          maxWidth: "220px",
-          display: isLoading ? "none" : "block",
-          margin: "0 auto",
-        }}
-        className="shadow"
-      />
     </div>
   );
 });
-
 
 const Carousel = ({ children }) => {
   const [active, setActive] = useState(0);
